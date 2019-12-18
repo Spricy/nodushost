cb0419b85cb3e76b9bcaab2c783858d951e7d8ec
Spricy <59005460+Spricy@users.noreply.github.com>
2019-12-18 00:15:23 -0500

    Add files via upload

---
+++ src/views/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <PageHeader title="Kahoot Cheats" />
+    <LoginForm/>
+  </div>
+</template>
+
+<script>
+import PageHeader from "@/components/PageHeader";
+import LoginForm from "@/components/Forms/LoginForm";
+
+export default {
+  components: {
+    PageHeader,
+    LoginForm
+  }
+};
+</script>
